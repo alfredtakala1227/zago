845e32a89214ebfa27f7919d710f6d4ba7e3c819
Jeremy Letto <jletto87@gmail.com>
2015-03-14 11:32:24 -0500

    attempted recovery

---
+++ site/js/app/modernizr-script.js
@@ -0,0 +1,11 @@
+// Give Modernizr.load a string, an object, or an array of strings and objects
+Modernizr.load([
+  {
+    test: Modernizr.mobile || Modernizr.phone || Modernizr.tablet,
+    yep: ['../css/touchscreens.css']
+  },
+  {
+    test: Modernizr.cssvhunit,
+    nope: ['../css/novhunits.css']
+  }
+]);
