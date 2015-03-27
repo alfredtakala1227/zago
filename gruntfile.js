44eed8c13f25eab50ca17472a5d56ba8a7712aa5
Jeremy Letto <jletto87@gmail.com>
2015-03-26 22:18:35 -0500

    gruntfile rebuild, img buggyfill conflict removed, img min-vh units changed to 100%

--- gruntfile.js
+++ gruntfile.js
@@ -1,5 +1,7 @@
 module.exports = function(grunt) {
 
+	grunt.loadNpmTasks('grunt-script-link-tags');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-compass');
 	grunt.loadNpmTasks('grunt-angular-templates');
@@ -7,6 +9,110 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-usemin');
 
 	grunt.initConfig({
+
+		compPath: 'components',
+
+		uglify: {
+
+            options: {
+                mangle: false,
+                compress: {
+                    drop_console: true,
+                    keep_fargs: true,
+                    hoist_funs: false,
+                    hoist_vars: false    
+                }
+            },
+
+            dependencies: {
+                files: {
+                    'site/js/lib/angular.js'    : [ 'components/js/lib/angular/**/*.js' ],
+                    'site/js/lib/jquery.js'     : [ 'components/js/lib/jquery/**/*.js' ],
+                    'site/js/lib/polyfills.js'  : [ 'components/js/lib/polyfills/**/*.js' ],
+                    'site/js/lib/start_scripts.js'  : [ 'components/js/lib/start_scripts/**/*.js' ],
+                    'site/js/lib/end_scripts.js'  : [ 
+                    	'components/js/lib/end_scripts/buggyfill/*.js',
+                    	'components/js/lib/end_scripts/buggyfill/modules/*.js'
+                    ],
+                    'site/js/lib/modernizr.js'  : [ 
+	                    	'components/js/lib/modernizr/*.js',
+	                    	'components/js/lib/modernizr/modules/*.js',
+	                    	'components/js/lib/modernizr/modules/modules/*.js',
+	                    	'components/js/lib/modernizr/modules/modules/modules*.js'
+	                    ]
+                }
+            },
+
+            dev: {
+                options: { 
+                    compress: false,
+                    beautify: true,
+                    expand: true,
+                },
+
+                files: {   /* Export main angular app file */
+                    'site/js/app/main.js' : [ 'components/js/app/**/*.js' ]
+                }
+            },
+
+            pro: { /* Compress and export application file + pieces */
+
+                files: {   /* Export main angular app file */
+                    'site/js/app/main.js' : [ 'components/js/app/**/*.js' ]
+                }
+            },
+
+        },
+
+        tags: {
+
+            options: {
+                scriptTemplate: '<script type="text/javascript" src="{{ path }}"></script>',
+                linkTemplate: '<link rel="stylesheet" type="text/css" href="{{ path }}">'
+            },
+
+            css: {
+                options: {
+                    openTag: '<!-- css files -->',
+                    closeTag: '<!-- /css files -->'
+                },
+                src: ['site/css/style.css'],
+                dest: 'site/index.html'
+            },
+
+            reqs: {
+                options: {
+                    openTag: '<!-- required files -->',
+                    closeTag: '<!-- /required files -->'
+                },
+                src: [ 
+                		'site/js/lib/polyfills.js',
+                		'site/js/lib/modernizr.js',
+                		'site/js/lib/jquery.js',
+                		'site/js/lib/angular.js',
+                		'site/js/lib/start_scripts.js'
+                	],
+                dest: 'site/index.html'
+            },
+
+            app: {
+                options: {
+                    openTag: '<!-- application files -->',
+                    closeTag: '<!-- /application files -->'
+                },
+                src: [ 'site/js/app/main.js' ],
+                dest: 'site/index.html'
+            },
+
+            end: {
+                options: {
+                    openTag: '<!-- closing files -->',
+                    closeTag: '<!-- /closing files -->'
+                },
+                src: [ 'site/js/lib/end_scripts.js' ],
+                dest: 'site/index.html'
+            }
+        },
 
 		compass: {
 			dev: {
@@ -32,7 +138,7 @@ module.exports = function(grunt) {
 			app:        {
 				cwd: 'site',
 				src:      ['*.html','pieces/*.html','partials/*.html'],
-				dest:     'site/js/app/templates.js',
+				dest:     'components/js/app/modules/templates.js',
 				options:  {
 					htmlmin: {
 						collapseBooleanAttributes:      true,
@@ -51,21 +157,30 @@ module.exports = function(grunt) {
 		watch: {
 			options: {livereload: true},
 
-			scripts: {
-				files: ['site/js/**/*.js'],
-				tasks: []
+			app: {
+				files: ['components/js/app/**/*.js'],
+				tasks: ['uglify:dev', 'tags:app']
+			},
+
+			deps: {
+				files: ['components/js/lib/**/*.js'],
+				tasks: ['uglify:dependencies', 'tags']
 			},
 			sass: {
-				files: ['components/**/*.scss'],
-				tasks: ['compass:dev', 'autoprefixer']
+				files: ['components/base.scss', 'components/sass/**/*.scss'],
+				tasks: ['compass:dev', 'autoprefixer', 'tags:css']
 			},
 			html: {
-				files: ['site/index.html', 'site/partials/*.html', 'site/pieces/*.html'],
+				files: ['site/partials/*.html', 'site/pieces/*.html'],
 				tasks: ['ngtemplates']
 			}
 		}
 
 	});
 
-	grunt.registerTask('default', ['compass', 'autoprefixer',  'ngtemplates', 'watch']);
+	grunt.registerTask('stack', ['compass', 'autoprefixer', 'ngtemplates', 'uglify:dependencies']);
+
+	grunt.registerTask('default', ['stack', 'uglify:dev', 'tags', 'watch']);
+	grunt.registerTask('build', ['stack', 'uglify:pro', 'tags']);
+
 }
