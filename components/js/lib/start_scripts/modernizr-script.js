25f4982236b7f20107e121680495b5c3e0c80c03
Jeremy Letto <jletto87@gmail.com>
2015-03-28 03:06:07 -0500

    remove obsolete classes, restructure some duplicate stuff, general cleanup

--- components/js/lib/start_scripts/modernizr-script.js
+++ components/js/lib/start_scripts/modernizr-script.js
@@ -3,9 +3,5 @@ Modernizr.load([
   {
     test: Modernizr.mobile || Modernizr.phone || Modernizr.tablet,
     yep: ['../css/touchscreens.css']
-  },
-  {
-    test: Modernizr.cssvhunit,
-    nope: ['../css/novhunits.css']
   }
 ]);
